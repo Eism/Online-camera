d4d5c3fab099b36d2d53be29ef3ea39847c15bf7
ELNUR-PC\ELNUR <eismailzada@gmail.com>
2016-10-20 23:44:23 +0200

    Menu и Окно "О программе"

--- OnlineCamera/app/src/main/java/com/example/onlinecamera/AppInfoActivity.java
+++ OnlineCamera/app/src/main/java/com/example/onlinecamera/AppInfoActivity.java
@@ -1,48 +1,40 @@
 package com.example.onlinecamera;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class BaseActivity extends AppCompatActivity {
+public class AppInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
+        setContentView(R.layout.activity_app_info);
 
         Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbal_actionbar);
         setSupportActionBar(mActionBarToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
-        setTitle("Онлайн-камеры");
+        setTitle("О программе");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         MenuItem coordMenuItem = menu.findItem(R.id.action_coord);
         coordMenuItem.setVisible(false);
 
+        MenuItem filterMenuItem = menu.findItem(R.id.action_filter);
+        filterMenuItem.setVisible(false);
+
+        MenuItem infoMenuItem = menu.findItem(R.id.action_app_info);
+        infoMenuItem.setVisible(false);
+
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id = item.getItemId();
 
-        switch (id){
-            case R.id.action_app_info:
-                Intent intent = new Intent(getApplicationContext(), AppInfoActivity.class);
-                startActivity(intent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
